59ec5645ee809469cfe885aa1979546191514209
MarcBollmann <dev@marcbollmann.de>
2020-11-23 13:44:24 +0100

    Test Commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ Strategy.prototype.userProfile = function (accessToken, done) {
         email,
         avatar: json.avatar,
         realm: this.options.realm,
-        adressStreet: json.adressStreet,
+        adressStreet: json.adress.street,
         adressHouseNumber: json.adressHouseNumber,
         adressPostalCode: json.adressPostalCode,
         adressCity: json.adressCity,
