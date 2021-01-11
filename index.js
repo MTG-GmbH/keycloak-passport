585118fb1bddbf3e56608825ec5670c3ed2c76d9
MarcBollmann <dev@marcbollmann.de>
2021-01-11 16:00:02 +0100

    Fixed undefined JSON Value Errors

--- index.js
+++ index.js
@@ -34,53 +34,96 @@ Strategy.prototype.userProfile = function (accessToken, done) {
 
     try {
       const json = JSON.parse(body);
+      Logger.log(json);
+
+
+      const keycloakId = json.sub;
+      const fullName = json.name;
+      const firstName = json.given_name;
+      const lastName = json.family_name;
+      const username = json.preferred_username;
       const email = json.email;
+      const avatar = json.avatar;
+      const realm = this.options.realm;
 
-      const userInfoDefault = {
-        addressStreet: "",
-        addressHouseNumber: "",
-        addressPostalCode: "",
-        addressCity: "",
-        addressCountry: "",
-        phoneBusinessMobile: "",
-        phoneBusinessInternal: "",
-        phoneBusinessLandline: "",
-        phonePrivateMobile: "",
-        phonePrivateHome: "",
+      let addressStreet = undefined;
+      let addressHouseNumber = undefined;
+      let addressPostalCode = undefined;
+      let addressCity = undefined;
+      let addressCountry = undefined;
+      let phoneBusinessMobile = undefined;
+      let phoneBusinessInternal = undefined;
+      let phoneBusinessLandline = undefined;
+      let phonePrivateMobile = undefined;
+      let phonePrivateHome = undefined;
+
+      let hasAddress = 'address' in json;
+      let hasPhone = 'phone' in json;
+      let hasPhoneBusiness = hasPhone && 'business' in json.phone;
+      let hasPhonePrivate = hasPhone && 'private' in json.phone;
+
+
+      if(hasAddress && 'street' in json.address) {
+        addressStreet = json.address.street;
       }
 
+      if(hasAddress && 'house_number' in json.address) {
+        addressHouseNumber = json.address.house_number;
+      }
 
-      const userInfoUnsafe = {
-        keycloakId: json.sub,
-        fullName: json.name,
-        firstName: json.given_name,
-        lastName: json.family_name,
-        username: json.preferred_username,
-        email,
-        avatar: json.avatar,
-        realm: this.options.realm,
-        addressStreet: json.address.street,
-        addressHouseNumber: json.address.house_number,
-        addressPostalCode: json.address.postal_code,
-        addressCity: json.address.city,
-        addressCountry: json.address.country,
-        phoneBusinessMobile: json.phone.business.mobile,
-        phoneBusinessInternal: json.phone.business.internal,
-        phoneBusinessLandline: json.phone.business.landline,
-        phonePrivateMobile: json.phone.private.mobile,
-        phonePrivateHome: json.phone.private.home,
-      };
+      if(hasAddress && 'postal_code' in json.address) {
+        addressPostalCode = json.address.postal_code;
+      }
 
+      if(hasAddress && 'city' in json.address) {
+        addressCity = json.address.city;
+      }
+
+      if(hasAddress && 'country' in json.address) {
+        addressCountry = json.address.country;
+      }
+
+      if(hasPhoneBusiness && 'mobile' in json.phone.business) {
+        phoneBusinessMobile = json.phone.business.mobile;
+      }
+
+      if(hasPhoneBusiness && 'internal' in json.phone.business) {
+        phoneBusinessInternal = json.phone.business.internal;
+      }
+
+      if(hasPhoneBusiness && 'landline' in json.phone.business) {
+        phoneBusinessLandline = json.phone.business.landline;
+      }
+
+      if(hasPhonePrivate && 'mobile' in json.phone.private) {
+        phonePrivateMobile = json.phone.private.mobile;
+      }
+
+      if(hasPhonePrivate && 'home' in json.phone.private) {
+        phonePrivateHome = json.phone.private.home;
+      }
+
+      
       const userInfo = {
-        ...userInfoUnsafe,
-        ...userInfoDefault,
+        keycloakId,
+        fullName,
+        firstName,
+        lastName,
+        username,
+        email,
+        avatar,
+        realm,
+        addressStreet,
+        addressHouseNumber,
+        addressPostalCode,
+        addressCity,
+        addressCountry,
+        phoneBusinessMobile,
+        phoneBusinessInternal,
+        phoneBusinessLandline,
+        phonePrivateMobile,
+        phonePrivateHome,
       };
-
-      console.log(userInfo);
-
-      for(var prop in userInfo) {
-        console.log("o." + prop + " = " + obj[prop]);
-      }
 
 
       done(null, userInfo);
